84638be86e45e6274f20235538e83fe6cbb8fef7
Darren Chan <darren@darchan0319@gmail.com>
2023-09-09 17:37:12 -0500

    fixing database initialization

--- backend/database/1-schemas.sql
+++ backend/database/1-schemas.sql
@@ -1,2 +1,2 @@
-DROP SCHEMA IF EXISTS bvb_accounts CASCADE;
-CREATE SCHEMA care_package;
+DROP SCHEMA IF EXISTS care_pledge CASCADE;
+CREATE SCHEMA care_pledge;
--- backend/database/2-users.sql
+++ backend/database/2-users.sql
@@ -1,29 +1,29 @@
 -- Create reader group
 CREATE ROLE readaccess LOGIN;
-GRANT USAGE ON SCHEMA care_package TO readaccess;
-ALTER DEFAULT PRIVILEGES IN SCHEMA care_package GRANT SELECT ON TABLES TO readaccess;
+GRANT USAGE ON SCHEMA care_pledge TO readaccess;
+ALTER DEFAULT PRIVILEGES IN SCHEMA care_pledge GRANT SELECT ON TABLES TO readaccess;
 
 -- Create Writer Group
 CREATE ROLE writeaccess LOGIN;
-GRANT USAGE ON SCHEMA care_package TO writeaccess;
-ALTER DEFAULT PRIVILEGES IN SCHEMA care_package GRANT SELECT, INSERT, UPDATE ON TABLES TO writeaccess;
+GRANT USAGE ON SCHEMA care_pledge TO writeaccess;
+ALTER DEFAULT PRIVILEGES IN SCHEMA care_pledge GRANT SELECT, INSERT, UPDATE ON TABLES TO writeaccess;
 
 -- Create Deletion Group
 CREATE ROLE deleteaccess LOGIN;
-GRANT USAGE ON SCHEMA care_package TO deleteaccess;
-ALTER DEFAULT PRIVILEGES IN SCHEMA care_package GRANT SELECT, DELETE ON TABLES TO deleteaccess;
+GRANT USAGE ON SCHEMA care_pledge TO deleteaccess;
+ALTER DEFAULT PRIVILEGES IN SCHEMA care_pledge GRANT SELECT, DELETE ON TABLES TO deleteaccess;
 
--- care_package reader
-DROP USER IF EXISTS care_package_reader;
-CREATE USER care_package_reader WITH PASSWORD 'reader123';
-GRANT readaccess TO care_package_reader;
+-- care_pledge reader
+DROP USER IF EXISTS care_pledge_reader;
+CREATE USER care_pledge_reader WITH PASSWORD 'reader123';
+GRANT readaccess TO care_pledge_reader;
 
--- care_package writer
-DROP USER IF EXISTS care_package_writer;
-CREATE USER care_package_writer WITH PASSWORD 'writer123';
-GRANT writeaccess TO care_package_writer;
+-- care_pledge writer
+DROP USER IF EXISTS care_pledge_writer;
+CREATE USER care_pledge_writer WITH PASSWORD 'writer123';
+GRANT writeaccess TO care_pledge_writer;
 
--- care_package remover
-DROP USER IF EXISTS care_package_remover;
-CREATE USER care_package_remover WITH PASSWORD 'remover123';
-GRANT deleteaccess TO care_package_remover;
+-- care_pledge remover
+DROP USER IF EXISTS care_pledge_remover;
+CREATE USER care_pledge_remover WITH PASSWORD 'remover123';
+GRANT deleteaccess TO care_pledge_remover;
