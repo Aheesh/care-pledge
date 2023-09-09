048609659f8387f0a4010a96eb423c95186ddd67
Darren Chan <darren@darchan0319@gmail.com>
2023-09-09 17:05:27 -0500

    working backend api with docker & postgres db

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,18 +1,17 @@
 import fastify from 'fastify';
 import helmet from 'fastify-helmet';
-import rateLimit from 'fastify-rate-limit';
-import { logger } from '../logs';
-import { setRateLimit } from './config/rate-limit';
-import { setDatabaseConnections } from './config/database/postgres/connection';
-import { routes } from './routes';
-import { TIME_WINDOW, MAX_LIMIT } from 'config';
+// import rateLimit from 'fastify-rate-limit';
+// import { setRateLimit } from './config/rate-limit';
+import { setDatabaseConnections } from './config/database/postgres/connection.js';
+import { routes } from './routes/index.js';
+// import { TIME_WINDOW, MAX_LIMIT } from '../config/index.js';
 
-const server = fastify({ logger: logger });
+const server = fastify();
 server.register(helmet);
-server.register(
-  rateLimit, 
-  setRateLimit(TIME_WINDOW, MAX_LIMIT)
-);
+// server.register(
+//   rateLimit, 
+//   setRateLimit(TIME_WINDOW, MAX_LIMIT)
+// );
 setDatabaseConnections(server);
 server.register(routes);
 
