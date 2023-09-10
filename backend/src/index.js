612871818a35b1bc1a16f1aa93ca4d83ec71860b
Darren Chan <darren@darchan0319@gmail.com>
2023-09-09 20:17:56 -0500

    added test service on front-end and cors to back-end

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,19 @@
 import fastify from 'fastify';
 import helmet from 'fastify-helmet';
+import cors from '@fastify/cors';
 // import rateLimit from 'fastify-rate-limit';
 // import { setRateLimit } from './config/rate-limit';
 import { setDatabaseConnections } from './config/database/postgres/connection.js';
 import { routes } from './routes/index.js';
-// import { TIME_WINDOW, MAX_LIMIT } from '../config/index.js';
 
+// import { TIME_WINDOW, MAX_LIMIT } from '../config/index.js';
 const server = fastify();
 server.register(helmet);
-// server.register(
-//   rateLimit, 
-//   setRateLimit(TIME_WINDOW, MAX_LIMIT)
-// );
+server.register(cors, { 
+  origin: true,
+  methods: ['GET', 'PUT', 'POST']
+});
+
 setDatabaseConnections(server);
 server.register(routes);
 
