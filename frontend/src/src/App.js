f2b3df9c8d8ff9cf1e499bb661420e724af2bbda
Darren Chan <darren@darchan0319@gmail.com>
2023-09-09 20:17:56 -0500

    added test service on front-end and cors to back-end

--- frontend/src/src/App.js
+++ frontend/src/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import {sendRequest} from './services/index.js';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={sendRequest}></button>
       </header>
     </div>
   );
